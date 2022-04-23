c325ade31ee3c8ecaa709856167aeda3eb1b7ba1
Valerio Leo <mail.valerioleo@gmail.com>
2022-04-23 20:16:43 +0200

    chore: remove console.log

--- src/js/app/src/data/subscription/subscriptionActions.ts
+++ src/js/app/src/data/subscription/subscriptionActions.ts
@@ -21,36 +21,16 @@ const subscribeService = async (
   const DAIContract = await sf.loadSuperToken('fDAIx');
   const DAI = DAIContract.address;
 
-  try {
-    const createFlowOperation = sf.cfaV1.createFlow({
-      receiver: recipient,
-      flowRate,
-      superToken: DAI
-      // userData?: string
-    });
+  const createFlowOperation = sf.cfaV1.createFlow({
+    receiver: recipient,
+    flowRate,
+    superToken: DAI
+    // userData?: string
+  });
 
-    console.log('Creating your stream...');
+  const result = await createFlowOperation.exec(signer);
 
-    const result = await createFlowOperation.exec(signer);
-    console.log(result);
-
-    console.log(
-      `Congrats - you've just created a money stream!
-    View Your Stream At: https://app.superfluid.finance/dashboard/${recipient}
-    Network: Kovan
-    Super Token: DAIx
-    Sender: 0xDCB45e4f6762C3D7C61a00e96Fb94ADb7Cf27721
-    Receiver: ${recipient},
-    FlowRate: ${flowRate}
-    `
-    );
-  }
-  catch(error) {
-    console.log(
-      'Hmmm, your transaction threw an error. Make sure that this stream does not already exist, and that you\'ve entered a valid Ethereum address!'
-    );
-    console.error(error);
-  }
+  return result;
 };
 
 // eslint-disable-next-line arrow-body-style
